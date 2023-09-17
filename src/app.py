5a34cfa15c1ed615d3ffa4e5c1d8ab050cc8e36f
Alexey <xayam@yandex.ru>
2023-09-17 09:31:38 +0300

    v3.7

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
 from model.model import Model
-from view.view import View
 from controller.controller import Controller
+from view.view import View
 
 
 class Application(Model, Controller, View):
     NAME = 'SyncBooker'
-    VERSION = '3.6alpha'
+    VERSION = '3.7'
 
     def __init__(self, debug=False):
         if not debug:
