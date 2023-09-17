853de5c424b6350bf45c75075c715f75e8bff014
Alexey <xayam@yandex.ru>
2023-09-17 22:40:27 +0300

    Add files via upload

--- src/android/main.py
+++ src/android/main.py
@@ -1,1 +1,10 @@
+from kivymd.app import MDApp
+from kivymd.uix.button import Button
 
+
+class MyApp(MDApp):
+  def build(self):
+    return Button(text="Hello World! Привет Мир!")
+
+
+MyApp().run()
